125e0751048dfad27ba8e4101957be10080ed70f
Nagao Shuhei <nash.peisan969@gmail.com>
2020-08-08 00:17:48 +0900

    application_controller.rb へUser "nickname" ストロングパラメータを記述

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  # CSRF対策で下記記述を追加
+  protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+    end
 end
